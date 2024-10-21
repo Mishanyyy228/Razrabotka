11d489ca7d20d6052a27bc5543d0c4ad1143859d
Mishanyyy228 <mananev13@gmail.com>
2024-10-21 11:36:11 +0300

    Добавьте файлы проекта.

---
+++ LogIn.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LB
+{
+    /// <summary>
+    /// Логика взаимодействия для LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new Registration();
+            Hide();
+            w2.Show();
+        }
+
+        private void Password_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password_user11.Text == "Введите пароль")
+            {
+                Password_user11.Text = string.Empty;
+                Password_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Password_user11_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Password_user11.Text == "")
+            {
+                Password_user11.Text = "Введите пароль";
+                Password_user11.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pochta_user11_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text == "student12@gmail.com")
+            {
+                Pochta_user11.Text = string.Empty;
+                Pochta_user11.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pochta_user11_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pochta_user11.Text == "")
+            {
+                Pochta_user11.Text = "mananev13@gmal.com";
+                Pochta_user11.Foreground = Brushes.Black;
+            }
+        }
+    }
+}
---
+++ Registration.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LB
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Name_user_GotFocus_1(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text == "Введите имя пользователя")
+            {
+                Name_user.Text = string.Empty;
+                Name_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Email_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Email_user.Text == "Введите почту")
+            {
+                Email_user.Text = string.Empty;
+                Email_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user.Text == "Введите пароль")
+            {
+                Pass_user.Text = string.Empty;
+                Pass_user.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Pass_user1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user1.Text == "Повторите пароль")
+            {
+                Pass_user1.Text = string.Empty;
+                Pass_user1.Foreground = Brushes.Black;
+            }
+        }
+
+        private void Name_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Name_user.Text == "")
+            {
+                Name_user.Text = "Введите имя пользователя";
+                Name_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Email_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Email_user.Text == "")
+            {
+                Email_user.Text = "Введите почту";
+                Email_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pass_user_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user.Text == "")
+            {
+                Pass_user.Text = "Введите пароль";
+                Pass_user.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Pass_user1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Pass_user1.Text == "")
+            {
+                Pass_user1.Text = "Повторите пароль";
+                Pass_user1.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window w2 = new LogIn();
+            Hide();
+            w2.Show();
+        }
+    }
+}
